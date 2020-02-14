115d3f381a96fe7a9529057059f760bb212067d0
itang <1094252227@qq.com>
2020-02-14 18:42:54 +0800

    项目完结！

--- src/main-dev.js
+++ src/main-dev.js
@@ -12,11 +12,22 @@ import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
+// 导入NProgress 包对应的js和css
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 import axios from 'axios'
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+// 在request 拦截器中，展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   // 必须返回config
+  return config
+})
+// 在response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
